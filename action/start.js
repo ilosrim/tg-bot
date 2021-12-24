d877d7e778b1a6e01082d62089829d911b032485
ilosrim <ilosrim@yahoo.com>
2021-12-25 00:44:57 +0500

    test bot

--- action/start.js
+++ action/start.js
@@ -1,5 +1,24 @@
 const {
+  Composer
+} = require('telegraf');
+const {
   bot
 } = require('../core/bot');
+const {
+  message
+} = require('../lib/message');
+const {
+  keyboards
+} = require('../lib/keyboard');
 
-bot.start(ctx => ctx.replyWithHTML(`Salom ${ctx.from.first_name}`).then());
+const composer = new Composer();
+
+composer.start(ctx => ctx.replyWithHTML(message["start"], {
+  reply_markup: keyboards["start"]
+}).then());
+
+composer.action("help", ctx => {
+  ctx.editedMessage(message["help"]).then()
+})
+
+bot.use(composer.middleware());
